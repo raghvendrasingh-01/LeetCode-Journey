b96c5c0b3f93b212d9a3b358b2d2eb2e42054bbb
Raghvendra Singh <raghvendrasingh1811@gmail.com>
2026-08-24 23:31:54 +0530

    Time: 1 ms (83.87%), Space: 42.5 MB (94.42%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -4,6 +4,7 @@ class Solution {
     if (words.length != pattern.length())
         return false;
     Map index = new HashMap();
+    
     for (Integer i=0; i<words.length; ++i)
         if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
             return false;
