ede188afe71f8a5822cc295fcf21f3927dbe7a45
Raghvendra Singh <raghvendrasingh1811@gmail.com>
2026-09-09 22:11:24 +0530

    Time: 4 ms (44.11%), Space: 46.4 MB (14.32%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,11 @@
+import java.util.TreeSet;
+class Solution {
+    public int thirdMax(int[] nums) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int num : nums) {
+            set.add(num);
+            if (set.size() > 3) set.pollFirst();
+        }
+        return set.size() == 3 ? set.first() : set.last();
+    }
+}
