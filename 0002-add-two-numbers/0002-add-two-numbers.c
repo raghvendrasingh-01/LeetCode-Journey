488724c22b38e86770b0836b459a71172ee6cf5a
Raghvendra Singh <raghvendrasingh1811@gmail.com>
2026-09-13 20:39:43 +0530

    Time: 2 ms (40.3%), Space: 13.6 MB (18.5%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.c
+++ 0002-add-two-numbers/0002-add-two-numbers.c
@@ -25,7 +25,6 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
         if (l1 != NULL) l1 = l1->next;
         if (l2 != NULL) l2 = l2->next;
     }
-
     return dummy.next;
 }
     
